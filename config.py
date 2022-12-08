ce8b82e0f36929c1a4c27f79c95f93e3c63ab3f4
FIN443 <jjhun1228@gmail.com>
2022-12-08 10:16:12 +0000

    wandb

---
+++ config.py
@@ -0,0 +1,17 @@
+from torch import cuda
+
+class Config:
+    data_dir='../input/data/ICDAR17_Korean'
+    model_dir='trained_models'
+    device='cuda' if cuda.is_available() else 'cpu'
+    num_workers=4
+    image_size=1024
+    input_size=512
+    batch_size=12
+    learning_rate=1e-3
+    max_epoch=200
+    save_interval=5
+    optimizer='Adam'
+    early_stopping=5
+    expr_name='config test'
+    resume_from='' # 이어서 학습 할 .pth 경로 없으면 ''
